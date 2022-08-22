d4dd89192089ffeb2e794702cb91945f532010b9
Akash giri <akgoswami857@gmail.com>
2022-08-22 23:01:58 +0530

    Update Q4.c

--- Assignment5/Q4.c
+++ Assignment5/Q4.c
@@ -6,7 +6,7 @@ int main(){
     int n;
     printf("enter a number: ");
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=n*2;i++){
         if(i%2!=0){
             printf("%d\n",i);
         }
